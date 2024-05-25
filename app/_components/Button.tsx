534e96082d3e736422abebacde9e2014ee652774
pedroarenas <parenas@nextlane.com>
2024-05-25 17:00:31 +0200

    refactor: cleaning a bot the structure

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -1,14 +1,12 @@
 export default function Button() {
 	return (
-		<div>
-			<button
-				className="border-2 box-border border-primary-blue 
+		<button
+			className="border-[1px] box-border border-primary-blue 
             shadow-lg hover:text-primary-blue hover:bg-custom-white 
             hover:border-primary-blue rounded-xl bg-primary-blue 
-            text-custom-white py-2 px-3 transition duration-300 ease-in-out w-[170px] h-[42px]"
-			>
-				Comprar
-			</button>
-		</div>
+            text-custom-white transition duration-300 ease-in-out w-[170px] h-[42px]"
+		>
+			Comprar
+		</button>
 	)
 }
