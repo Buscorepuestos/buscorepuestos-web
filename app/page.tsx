e4a1ebfafbc0f65eb7b853306d3798dc5395e90c
Pedro Arenas <antonioaren2@gmail.com>
2024-06-02 16:46:14 +0200

    fix: [BR-337] Deleted wrongg stuff. Reverting header, footer component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 import React, { createContext } from 'react'
 import { SwiperSlide } from 'swiper/react'
-import BannerImage from '@/app/(core)/components/BannerImage'
-import Button from '@/app/(core)/components/Button'
-import CardInfo from '@/app/(core)/components/cards/CardInfo'
-import SearchBar from '@/app/(core)/components/SearchBar'
-import Slider from '@/app/(core)/components/Slider'
-import CardPrice from '@/app/(core)/components/cards/CardPrice'
-import CardValoration from '@/app/(core)/components/cards/CardValoration'
+import BannerImage from '@/app/core/components/BannerImage'
+import Button from '@/app/core/components/Button'
+import CardInfo from '@/app/core/components/cards/CardInfo'
+import SearchBar from '@/app/core/components/SearchBar'
+import Slider from '@/app/core/components/Slider'
+import CardPrice from '@/app/core/components/cards/CardPrice'
+import CardValoration from '@/app/core/components/cards/CardValoration'
 
 const cardInfoPropsArray = [
 	{
