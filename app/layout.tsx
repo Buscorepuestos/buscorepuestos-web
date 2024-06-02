ad9cda32bf90777cd78561c9ad48fc6a9c9a7278
Pedro Arenas <antonioaren2@gmail.com>
2024-06-02 17:02:21 +0200

    feat: [BR-337] Added analytics and speedInsight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
 
 import { Header } from '@/app/core/components/global/header'
@@ -30,6 +32,8 @@ export default function RootLayout({
 			<body
 				className={`${bebasNeue.variable} ${workSans.variable} flex flex-col justify-center items-center `}
 			>
+				<Analytics/>
+				<SpeedInsights/>
 				<Header />
 				{children}
 				<Footer />
