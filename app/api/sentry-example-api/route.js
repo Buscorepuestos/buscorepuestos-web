a3e1bec41cc6f1e704a62c499da610d429bf7bf3
Pedro Arenas <antonioaren2@gmail.com>
2024-06-02 22:56:02 +0200

    feat: [BR-337] Coverage limit forcing testing. This commit should fail.

--- app/api/sentry-example-api/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export const dynamic = "force-dynamic";
-
-// A faulty API route to __tests__ Sentry's error monitoring
-export function GET() {
-  throw new Error("Sentry Example API Route Error");
-  return NextResponse.json({ data: "Testing Sentry Error..." });
-}
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
- 
+
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-  },
+	plugins: [react()],
+	test: {
+		environment: 'jsdom',
+		coverage: {
+			thresholds: {
+				statements: 60,
+				branches: 60,
+				functions: 60,
+				lines: 60,
+			},
+			provider: 'v8',
+			ignoreEmptyLines: true,
+			include: ['app/**/*'],
+			exclude: ['node_modules', 'test/**/*', 'app/**/*.test.*', 'app/core/components/svg', 'app/component-examples'],
+			reporter: ['text', 'html', 'json'],
+		},
+	},
 })
