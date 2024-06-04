8632ca669ca2a8a6c81e9eb3fb831aee5992b999
Pedro Arenas <antonioaren2@gmail.com>
2024-06-04 21:32:02 +0200

    test: [BR-341] Header test completed.

---
+++ app/core/components/__tests__/header.test.tsx
@@ -0,0 +1,41 @@
+import { expect, test, describe, afterEach } from 'vitest'
+import { cleanup, render, screen, within } from '@testing-library/react'
+import { Header } from '../global/header'
+
+const principalMenuLinks = [
+	{ label: 'Tienda', href: '#' },
+	{ label: 'Quienes somos', href: '#' },
+	{ label: 'Ayuda', href: '#' },
+	{ label: 'Contacto', href: '#' },
+]
+const secondaryMenuLinks = [
+	{ label: 'Marcas', href: '#' },
+	{ label: 'Categorías', href: '#' },
+	{ label: 'Accesorios', href: '#' },
+	{ label: 'Alumbrado', href: '#' },
+	{ label: 'Cambio/Embrague', href: '#' },
+	{ label: 'Carrocería', href: '#' },
+	{ label: 'Climatización', href: '#' },
+	{ label: 'Electricidad', href: '#' },
+	{ label: 'Motor', href: '#' },
+]
+
+describe('Header component', () => {
+	afterEach(() => {
+		cleanup()
+	})
+	test('Slider content elements', () => {
+		render(<Header />)
+		// check if render img logo
+		expect(screen.getByAltText('Header')).toBeDefined()
+
+	})
+	test('All menu links', () => {
+		render(<Header />)
+		const allLinks = [...principalMenuLinks, ...secondaryMenuLinks]
+
+		allLinks.forEach(link => {
+			expect(screen.getByText(link.label)).toBeDefined()
+		})
+	})
+})
