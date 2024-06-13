578c1fe23f36796485a57018ad8bb1f51482f406
ylavin <ylavin@gentalia.es>
2024-06-13 13:20:31 -0400

    chore: update props use and style for component

--- app/core/components/Banner.tsx
+++ app/core/components/Banner.tsx
@@ -4,20 +4,31 @@ function getAligned(aligned: 'center' | 'left' | 'right' | undefined) {
 	return 'justify-center'
 }
 
+interface BannerStyle {
+	backgroundImage?: string,
+	height: string,
+	justifyContent: string,
+	backgroundColor: string
+}
+
 export default function Banner(props: {
-	imgUrl: string
 	height: string
+	color: string
+	imgUrl?: string
 	aligned?: 'center' | 'left' | 'right'
 	children?: React.ReactNode
 }) {
+	const styles: BannerStyle = {
+		backgroundColor: "blue",
+		height: props.height,
+		justifyContent: getAligned(props.aligned),
+	};
+	if (props.imgUrl)
+		styles.backgroundImage = `url(${props.imgUrl})`;
 	return (
 		<section
 			className={`w-full bg-no-repeat bg-cover bg-center flex justify-center`}
-			style={{
-				backgroundImage: `url(${props.imgUrl})`,
-				height: props.height,
-				justifyContent: getAligned(props.aligned),
-			}}
+			style={styles}
 		>
 			{props.children}
 		</section>
