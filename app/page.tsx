b36738cf1b896ea1d9285481a8a249b2401623c6
ylavin <ylavin@gentalia.es>
2024-06-18 14:25:39 -0400

    feat: add mobile view for banner Values component

--- app/page.tsx
+++ app/page.tsx
@@ -210,12 +210,12 @@ export default function Home() {
 				</div>
 			</section>
 
-			<section>
 				<Banner imgUrl="/banner-warehouse.webp"
 						height="305px"
 						aligned="left"
 						color="blue"
-						position="absolute"
+						position="desktop:absolute"
+						extraCss={'mobile:hidden'}
 				>
 					<div className="flex justify-center">
 						<h1 className="content-center text-8xl text-custom-white text-left ml-[150px] max-w-[450px] mobile:max-w-[350px] mobile:text-xl">
@@ -223,31 +223,78 @@ export default function Home() {
 						</h1>
 					</div>
 				</Banner>
-				<div className="flex flex-col items-end relative bottom-12">
+				<section className="flex flex-col items-end relative bottom-12 mobile:hidden">
 					<TagBanner title={'Rapidez'} fontColor={'white'} color={'#3A27B0'} width={'1000px'} height={'130px'}
-							   align={'center'} position={'relative'} zIndex={'1'}
+							   align={'center'} alignItems={'center'} position={'relative'} zIndex={'1'}
 							   radius={'rounded-tl-3xl rounded-bl-3xl'}>
-						<p className="w-[40vw] text-title-4">Con solo unos clics, nos encargamos de buscar el repuesto que necesitas
+						<p className="w-[40vw] text-title-4">Con solo unos clics, nos encargamos de buscar el repuesto
+							que necesitas
 							de forma rápida y
 							fácil.
 							Comparamos entre el stock de cientos de tiendas para garantizar la mejor opción en tiempo
 							récord.</p>
 					</TagBanner>
 					<TagBanner title={'Eficacia'} fontColor={'white'} color={'#12B1BB'} width={'870px'}
-							   height={'130px'} align={'center'} position={'relative'} zIndex={'2'} axis={'bottom-5'}
+							   height={'130px'} align={'center'} alignItems={'center'} position={'relative'} zIndex={'2'} axis={'bottom-5'}
 							   radius={'rounded-tl-3xl rounded-bl-3xl'}>
-						<p className="w-[35vw] text-title-4">Todas nuestras piezas han sido validadas por profesionales, es por ello
+						<p className="w-[35vw] text-title-4">Todas nuestras piezas han sido validadas por profesionales,
+							es por ello
 							que tenemos el
-							porcentaje de devoluciones más bajo del mercado. </p>
+							porcentaje de devoluciones más bajo del mercado.</p>
 					</TagBanner>
 					<TagBanner title={'100% Fiabilidad'} fontColor={'white'} color={'#1A61F1'} width={'650px'}
-							   height={'130px'} align={'center'} position={'relative'} zIndex={'2'} axis={'bottom-9'}
+							   height={'130px'} align={'center'} alignItems={'center'} position={'relative'} zIndex={'2'} axis={'bottom-9'}
 							   radius={'rounded-tl-3xl rounded-bl-3xl'}>
 						<p className="w-[25vw] text-title-4">Devoluciones sin coste en el plazo de 14 días.
 							Tu satisfacción y confianza son nuestra prioridad.</p>
 					</TagBanner>
-				</div>
-			</section>
+				</section>
+
+				<Banner imgUrl="/mobile_banner_warehouse.webp"
+						height="920px"
+						aligned="center"
+						color="blue"
+						position={''}
+						extraCss={'desktop:hidden m-auto items-start'}
+				>
+					<div className="flex justify-center">
+						<h1 className="content-center text-8xl text-custom-white text-left w-[65vw] mt-16 mobile:text-xl">
+							Busca repuestos inter<span className="text-warning">nacionales</span> y compara precios
+						</h1>
+					</div>
+				</Banner>
+				<section className="desktop:hidden flex justify-center relative z-10" style={{ bottom: '700px' }}>
+					<div className="flex flex-col align-end w-[100vw]">
+						<TagBanner title={'Rapidez'} flexDir={'flex-col'} fontColor={'white'} color={'#3A27B0'}
+								   width={'90vw'} height={'auto'}
+								   align={'left'} alignItems={'left'} alignSelf={'self-end'}
+								   radius={'rounded-tl-3xl rounded-bl-3xl'} padding={'p-6 pl-12'}>
+							<p className="text-title-4 font-light">Con solo unos clics, nos encargamos de buscar el
+								repuesto que necesitas
+								de forma rápida y
+								fácil.
+								<span className={'font-semibold'}>Comparamos entre el stock de cientos de tiendas para garantizar la mejor opción en
+								tiempo
+									récord</span>.</p>
+						</TagBanner>
+						<TagBanner title={'Eficacia'} flexDir={'flex-col'} fontColor={'white'} color={'#12B1BB'}
+								   width={'80vw'} height={'auto'}
+								   align={'left'} alignItems={'left'} alignSelf={'self-end'}
+								   radius={'rounded-tl-3xl rounded-bl-3xl'} padding={'p-6 pl-12'}>
+							<p className="text-title-4 font-light"><span className={'font-semibold'}>Todas nuestras piezas han sido validadas por profesionales</span>,
+								es por ello
+								que tenemos el
+								porcentaje de devoluciones más bajo del mercado.</p>
+						</TagBanner>
+						<TagBanner title={'100% Fiabilidad'} flexDir={'flex-col'} fontColor={'white'} color={'#1A61F1'}
+								   width={'70vw'} height={'auto'}
+								   align={'left'} alignItems={'left'} alignSelf={'self-end'}
+								   radius={'rounded-tl-3xl rounded-bl-3xl'} padding={'p-6 pl-12'}>
+							<p className="text-title-4 font-light">Devoluciones sin coste en el plazo de <span className={'font-semibold'}>14 días.
+								Tu satisfacción y confianza son nuestra prioridad.</span></p>
+						</TagBanner>
+					</div>
+				</section>
 
 			<section className="pt-[72px]">
 				<h2 className="text-title-2 mb-[46px]"> Novedades</h2>
