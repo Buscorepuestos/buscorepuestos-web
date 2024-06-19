16ec7219ad1df2bb211c7f9e9348ff7ae3ebc8d2
Jhoskar Toro <jhoskartoro@gmail.com>
2024-06-19 11:43:07 -0400

    style: :art: [BR-356] Parametrizacion de componente button
    Se parametriza el background, color de letra, hovers de background y de letra, y borderColor para personalizar aun mas el componente button

--- app/core/components/Button.tsx
+++ app/core/components/Button.tsx
@@ -1,48 +1,76 @@
 export default function Button(props: {
 	labelName?: string
 	type?: 'primary' | 'secondary' | 'tertiary'
-	hover?: boolean
+	hoverBg?: string
+	hoverText?: string
+	bg?: string
+	color?: string
+	borderColor?: string
 }) {
-	const { 
-		labelName = 'Comprar', 
-		type = 'primary'
-	} = props
+	
+	if (props.type === 'secondary') {
 
-	if (type === 'secondary') {
+		const { 
+			labelName = 'Añadir a la cesta', 
+			bg = 'bg-secondary-blue',
+			hoverBg = 'hover:bg-custom-white',
+			hoverText = 'hover:text-secondary-blue',
+			borderColor = 'border-secondary-blue'
+		} = props
+
 		return (
 			<button
-			className="
-				border-[2px] border-secondary-blue box-border shadow-lg hover:text-secondary-blue hover:bg-custom-white 
-				rounded-3xl bg-secondary-blue text-custom-white 
-				transition duration-300 ease-in-out w-[170px] h-[42px]
-				font-tertiary-font text-[0.9vw]
-				"
+				className={`
+						${bg} ${borderColor} border-[2px] box-border shadow-lg 
+						${hoverText} ${hoverBg} 
+						rounded-3xl text-custom-white 
+						transition duration-300 ease-in-out w-[170px] h-[42px]
+						font-tertiary-font text-[3vw] lg:text-[1vw] md:text-[2vw] 
+					`}
 			>
 				{labelName}
 			</button>
 		)
-	} else if (type === 'tertiary') {
+	} else if (props.type === 'tertiary') {
+
+		const { 
+			labelName = 'Siguiente', 
+			bg = 'bg-secondary-blue',
+			hoverBg = 'hover:bg-custom-white',
+			hoverText = 'hover:text-secondary-blue',
+			borderColor = 'border-secondary-blue'
+		} = props
+		
 		return (
 			<button
-				className="
-					border-[2px] border-secondary-blue box-border shadow-lg hover:text-secondary-blue hover:bg-custom-white 
-					rounded-3xl bg-secondary-blue text-custom-white 
+				className={`
+					${bg} ${borderColor} border-[2px] box-border shadow-lg 
+					${hoverText} ${hoverBg}
+					rounded-3xl text-custom-white 
 					transition duration-300 ease-in-out w-[170px] h-[35px]
-					font-tertiary-font text-[0.9vw]
-				"
+					font-tertiary-font text-[3vw] lg:text-[1vw] md:text-[2vw]
+				`}
 			>
 				{labelName}
 			</button>
 		)
 	}
+
+	const { 
+		labelName = 'Comprar', 
+		bg = 'bg-primary-blue',
+		hoverBg = 'hover:bg-primary-lila',
+		hoverText = 'hover:text-white',
+	} = props
+
 	return (
 		<button
 			className={` 
-				border-[1px] box-border shadow-lg hover:text-white 
-				hover:bg-primary-lila rounded-3xl bg-primary-blue
+				${bg} border-[1px] box-border shadow-lg
+				${hoverText} ${hoverBg} rounded-3xl
 				text-custom-white transition duration-300 
 				ease-in-out w-[170px] h-[42px]
-				font-tertiary-font text-[0.9vw] font-semibold
+				font-tertiary-font text-[3vw] lg:text-[1vw] md:text-[2vw]
 			`}
 		>
 			{labelName}
--- app/core/components/cards/CardPrice.tsx
+++ app/core/components/cards/CardPrice.tsx
@@ -43,7 +43,7 @@ export default function CardPrice(props: {
 						<PriceView price={props.price} />
 					</div>
 					<div className='flex justify-center w-[100%]'>
-						<Button type='tertiary' labelName='Volver'/>
+						<Button type='primary'/>
 					</div>
 				</div>
 			</div>
