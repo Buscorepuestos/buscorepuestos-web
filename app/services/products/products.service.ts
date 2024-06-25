d60dbbc4cb278efb1e190c884d8c27c7cb90891b
ylavin <ylavin@gentalia.es>
2024-06-25 12:36:47 -0400

    refactor: return result as Product type array

--- app/services/products/products.service.ts
+++ app/services/products/products.service.ts
@@ -1,9 +1,10 @@
 import api from '../../api/api'
+import { IProductMongoose } from '../../types/product'
 
-export const getProducts = async (size: number = 16, sort: string = 'created', order: string = 'desc'): Promise<T> => {
+export const getProducts = async (size: number = 116, sort: string = 'created', order: string = 'desc'): Promise<IProductMongoose[]> => {
 	try {
 		const response = await api.get<any[]>(`/products/store?size=${size}&sort=${sort}&order=${order}`)
-		return response.data.data.selectedProducts
+		return response.data.data.selectedProducts as IProductMongoose[]
 	} catch (error) {
 		console.error('Error fetching products:', error)
 		throw error
