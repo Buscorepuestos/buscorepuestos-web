4bc47e3da05f169ff3f6e638476edcc8e6c5e1a7
ylavin <ylavin@gentalia.es>
2024-06-25 12:39:17 -0400

    feat: add index page for Store section

---
+++ app/store/page.tsx
@@ -0,0 +1,45 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import CardPrice from '../core/components/cards/CardPrice'
+import { getProducts } from '../services/products/products.service'
+import { IProductMongoose } from '../types/product'
+
+export default function Store() {
+
+	const [products, setProducts] = useState<IProductMongoose[]>([])
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState<string | null>(null)
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			try {
+				const data = await getProducts()
+				console.log(data)
+				setProducts(data)
+			} catch (error) {
+				setError((error as Error).message)
+			} finally {
+				setLoading(false)
+			}
+		}
+
+		fetchProducts()
+	}, [])
+
+	if (loading) return <p>Loading...</p>
+	if (error) return <p>Error: {error}</p>
+
+	return (
+		<main>
+			<section className={'grid grid-cols-4 grid-rows-4 tablet:grid-cols-3 tablet:grid-rows-3 mobile:grid-cols-2 mobile:grid-rows-2'}>
+				{products.map((product: any) => (
+					<CardPrice title={product.title}
+							   reference={product.mainReference}
+							   description={`${product.brand}${" "+product.articleModel}${" "+product.year}`}
+							   price={product.buscorepuestosPrice.toString().replace('.', ',')}
+							   image={product.images[0]} />
+				))}
+			</section>
+		</main>
+	)
+}
