2f1cfd3b3f67bb5028c0e0424eceee9039bc2fc4
ylavin <ylavin@gentalia.es>
2024-06-25 15:31:57 -0400

    chore: set default size of result

--- app/services/products/products.service.ts
+++ app/services/products/products.service.ts
@@ -1,7 +1,7 @@
 import api from '../../api/api'
 import { AxiosResponse } from 'axios'
 
-export const getProducts = async (size: number = 116, sort: string = 'created', order: string = 'desc'): Promise<AxiosResponse<T>> => {
+export const getProducts = async (size: number = 16, sort: string = 'created', order: string = 'desc'): Promise<AxiosResponse<T>> => {
 	try {
 		return await api.get(`/products/store?size=${size}&sort=${sort}&order=${order}`)
 	} catch (error) {
