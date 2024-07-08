a5fd4ea27bf253731067a99a9f6e84dbf0fd2ce5
Jhoskar Toro <jhoskartoro@gmail.com>
2024-07-08 17:37:24 -0400

    style: :art: [BR-448] Estilos de fondo de pagina producto, y fondo done va la info del producto

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -43,6 +43,7 @@ const config: Config = {
 				'dark-grey': 'var(--dark-grey)',
 				'light-grey': 'var(--light-grey)',
 				'neutro-100': 'var(--neutro100)',
+				'neutro-grey': 'var(--neutro-grey)',
 			},
 			fontFamily: {
 				'primary-font': ['var(--font-work-sans)'],
