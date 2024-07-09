7b0ced8fd49b04f278a6e42a24300143ca03b5b7
Jhoskar Toro <jhoskartoro@gmail.com>
2024-07-09 15:13:48 -0400

    style: :art: [BR-448] Correcion de botones, margenes de componentes y version responsive de componente product-price

--- app/core/components/productPrice/productPrice.tsx
+++ app/core/components/productPrice/productPrice.tsx
@@ -5,24 +5,25 @@ import Button from '../Button'
 const productPrice = () => {
     return (
         <div className='flex flex-col justify-center items-center font-tertiary-font'>
-            <p className='text-[32px] text-primary-blue font-semibold'>
+            <p className='text-[32px] xl:text-[2.5vw] lg:text-[2.8vw] md:text-[3.2vw] sm:text-[3.5vw] text-primary-blue font-semibold'>
                 148,12€
             </p>
-            <p className='font-semibold'>
+            <p className='font-semibold xl:text-[1vw] sm:text-[1.8vw]'>
                 Envío e IVA incluido
             </p>
-            <div className='flex text-custom-orange gap-3'>
+            <div className='text-custom-orange gap-3 flex items-center'>
                 <Image
                     src='/info.svg'
                     alt='warning'
                     width={20}
                     height={20}
+                    className='sm:w-[17px] sm:h-[17px] md:w-[22px] md:h-[22px]'
                 />
-                <p>
+                <p className='text-[2.8vw] xl:text-[1vw] md:text-[1.3vw] sm:text-[1.4vw]'>
                     Precio medio pieza original nueva:  
                     <span className='font-semibold line-through'> 200,00 € </span>
                 </p>
-                <p className='bg-custom-orange text-custom-white rounded-2xl px-1'>
+                <p className='text-[2.8vw] xl:text-[1vw] md:text-[1.3vw] sm:text-[1.4vw] bg-custom-orange text-custom-white rounded-2xl px-1 flex items-center'>
                     -10%
                 </p>
             </div>
