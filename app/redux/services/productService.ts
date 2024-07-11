347aee1e7495b63a5e8c5dd0da846ddd2cd663a4
Jhoskar Toro <jhoskartoro@gmail.com>
2024-07-11 12:47:30 -0400

    feat: :zap: [BR-518] Configuracion global de redux-toolkit y primera configuracion de peticion para producto

---
+++ app/redux/services/productService.ts
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { ProductMongoInterface } from "../interfaces/product.interface";
+
+export const productsApi = createApi({
+    reducerPath: "productsApi",
+    baseQuery: fetchBaseQuery({ baseUrl: "https://buscorepuestos-dev.herokuapp.com/api" }),
+    tagTypes: ["Product"],
+    endpoints: (builder) => ({
+        getProductById: builder.query<ProductMongoInterface, {id: string}>({
+            query: ({id}) => `products/product-mongo/${id}`,
+            providesTags: ["Product"],
+        }),
+    }),
+});
+
+export const { useGetProductByIdQuery } = productsApi;
