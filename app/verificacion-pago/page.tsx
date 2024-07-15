7714c6aa5033e8cd49fbc777eec7b991719d5c98
Yuniel Lavin <yuniel.lavin@buscorepuestos.com>
2024-07-15 14:56:13 -0400

    feat: [BR-479] shipping address section

--- app/verificacion-pago/page.tsx
+++ app/verificacion-pago/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 import ShoppingBasket from '../core/components/shopping-cart/ShoppingBasket'
-import React from 'react'
+import React, { useState } from 'react'
 import { p } from 'vitest/dist/reporters-yx5ZTtEV'
 import Input from '../core/components/input/input'
+import SelectDropdown from '../core/components/selectDropdown/selectDropdown'
+import Checkbox from '../core/components/checkbox/checkbox'
 
 export default function Payment() {
+	const [sameBillAddress, setSameBillAddress] = useState<boolean>(false);
 
 	const products = [
 		{
@@ -56,8 +59,19 @@ export default function Payment() {
 			price: 156,
 			isMobile: false,
 			isAvailable: true,
-		}
+		},
 	]
+
+	const options = [
+		{ value: 'opcion1', label: 'Direcciones de envío guardadas' },
+		{ value: 'option2', label: 'Option 2' },
+		{ value: 'option3', label: 'Option 3' }
+	];
+
+	const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSameBillAddress(event.target.checked);
+	};
+
 	return (
 		<section className={'w-full bg-green-300 mt-[12vw] mb-[8vw] p-6'}>
 			<article>
@@ -71,20 +85,45 @@ export default function Payment() {
 			</article>
 			<article>
 				<form className={'flex flex-col justify-center items-center'}>
-					<h1 className={'text-title-3 mb-6 self-start'}>Información personal</h1>
+					{/*Personal Information*/}
+					<h1 className={'text-title-3 mb-4 self-start'}>Información personal</h1>
 					<div className={'grid grid-cols-1 w-[60%] gap-4'}>
-						<Input placeholder={'Nombre y apellidos'} name={'name'} value={''}/>
+						<Input placeholder={'Nombre y apellidos'} name={'name'} value={''} />
 					</div>
 					<div className={'grid grid-cols-3 w-[60%] mt-4 gap-4'}>
 						<Input placeholder={'Email'} name={'email'} value={''} cssClass={'col-span-2'} />
-						<Input placeholder={'NIF / CIF'} name={'company_id'} value={''}/>
+						<Input placeholder={'NIF / CIF'} name={'company_id'} value={''} />
 					</div>
 					<div className={'grid grid-cols-2 w-[60%] mt-4 gap-4'}>
-						<Input placeholder={'Número de teléfono'} name={'phone'} value={''}/>
+						<Input placeholder={'Número de teléfono'} name={'phone'} value={''} />
+					</div>
+					{/*Shipping Address Information*/}
+					<h1 className={'text-title-3 mb-4 mt-12 self-start'}>Dirección de envío</h1>
+					<div className={'grid grid-cols-3 w-[60%] gap-4'}>
+						<div className={'col-span-2'}>
+							<SelectDropdown name={'saved_address'} options={options}
+											 placeholder={'Direcciones de envío guardadas'} />
+						</div>
+					</div>
+					<div className={'grid grid-cols-1 w-[60%] mt-4 gap-4'}>
+						<Input placeholder={'Dirección'} name={'address_name'} value={''} />
+					</div>
+					<div className={'grid grid-cols-1 w-[60%] mt-4 gap-4'}>
+						<Input placeholder={'Número, piso, puerta, portal'} name={'address_extra'} value={''} />
+					</div>
+					<div className={'grid grid-cols-2 w-[60%] mt-4 gap-4'}>
+						<Input placeholder={'Código postal'} name={'zip'} value={''}  />
+						<Input placeholder={'Ciudad'} name={'city'} value={''} />
+					</div>
+					<div className={'grid grid-cols-2 w-[60%] mt-4 gap-4'}>
+						<Input placeholder={'Provincia'} name={'province'} value={''}  />
+						<Input placeholder={'País'} name={'country'} value={''} />
+					</div>
+					<div className={'grid grid-cols-1 w-[60%] mt-4 gap-4'}>
+						<Checkbox name={'same_address'} value={sameBillAddress} onChange={handleCheckboxChange} label={'Usar la misma dirección de facturación'} />
 					</div>
 				</form>
 			</article>
-
 		</section>
 	)
 }
