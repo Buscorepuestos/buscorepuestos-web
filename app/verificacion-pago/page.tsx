69fbf018c8240dba418eca13224936ab9d0e967d
Yuniel Lavin <yuniel.lavin@buscorepuestos.com>
2024-07-15 16:48:25 -0400

    feat: [BR-479] add stripe form at payment verification form

--- app/verificacion-pago/page.tsx
+++ app/verificacion-pago/page.tsx
@@ -1,13 +1,37 @@
 'use client'
 import ShoppingBasket from '../core/components/shopping-cart/ShoppingBasket'
-import React, { useState } from 'react'
-import { p } from 'vitest/dist/reporters-yx5ZTtEV'
+import React, { useEffect, useState } from 'react'
 import Input from '../core/components/input/input'
 import SelectDropdown from '../core/components/selectDropdown/selectDropdown'
 import Checkbox from '../core/components/checkbox/checkbox'
+import PaymentForm from '../core/components/checkout/PaymentForm'
+import { createPaymentIntent } from '../services/checkout/stripe.service'
+import './stripe.css';
 
 export default function Payment() {
 	const [sameBillAddress, setSameBillAddress] = useState<boolean>(false);
+	const [clientSecret, setClientSecret] = useState('')
+	const [error, setError] = useState('')
+
+	useEffect(() => {
+		const createIntent = () => {
+			try {
+				// Mock data
+				createPaymentIntent({
+					amount: 200,
+					currency: 'eur',
+					cartIDs: ['recV9AQVCb64NveMF'],
+					automatic_payment_methods: { enabled: true },
+				}).then((res) =>
+					setClientSecret(res.data.client_secret),
+				)
+			} catch (error) {
+				setError(error)
+			}
+		}
+
+		createIntent()
+	}, [])
 
 	const products = [
 		{
@@ -79,7 +103,7 @@ export default function Payment() {
 	};
 
 	return (
-		<section className={'w-full bg-green-300 mt-[12vw] mb-[8vw] p-6'}>
+		<section className={'w-full bg-custom-white rounded-[10px] shadow-lg mt-[12vw] mb-[8vw] p-6'}>
 			<article>
 				<ShoppingBasket products={products} />
 				<div className="w-full h-[2px] bg-secondary-blue mt-6" />
@@ -92,7 +116,7 @@ export default function Payment() {
 			<article>
 				<form className={'flex flex-col justify-center items-center'}>
 					{/*Personal Information*/}
-					<h1 className={'text-title-3 mb-4 self-start'}>Información personal</h1>
+					<h1 className={'text-title-4 font-tertiary-font mb-4 self-start'}>Información personal</h1>
 					<div className={'grid grid-cols-1 w-[60%] gap-4'}>
 						<Input placeholder={'Nombre y apellidos'} name={'name'} value={''} />
 					</div>
@@ -105,7 +129,7 @@ export default function Payment() {
 					</div>
 
 					{/*Shipping Address Information*/}
-					<h1 className={'text-title-3 mb-4 mt-12 self-start'}>Dirección de envío</h1>
+					<h1 className={'text-title-4 font-tertiary-font mb-4 mt-12 self-start'}>Dirección de envío</h1>
 					<div className={'grid grid-cols-3 w-[60%] gap-4'}>
 						<div className={'col-span-2'}>
 							<SelectDropdown name={'saved_shipping_address'} options={shippingOptions}
@@ -131,7 +155,7 @@ export default function Payment() {
 					</div>
 
 					{/*Billing address*/}
-					<h1 className={'text-title-3 mb-4 mt-12 self-start'}>Dirección de facturación</h1>
+					<h1 className={'text-title-4 font-tertiary-font mb-4 mt-12 self-start'}>Dirección de facturación</h1>
 					<div className={'grid grid-cols-3 w-[60%] gap-4'}>
 						<div className={'col-span-2'}>
 							<SelectDropdown name={'saved_billing_address'} options={billingOptions}
@@ -148,6 +172,13 @@ export default function Payment() {
 						<Input placeholder={'Código postal'} name={'bill_zip'} value={''}  />
 						<Input placeholder={'Provincia'} name={'bill_province'} value={''}  />
 					</div>
+
+					{/*Stripe Form*/}
+					<h1 className={'text-title-4 font-tertiary-font mb-4 mt-12 self-start'}>Método de pago</h1>
+					<div>
+						{clientSecret && <PaymentForm clientSecret={clientSecret} />}
+					</div>
+
 				</form>
 			</article>
 		</section>
