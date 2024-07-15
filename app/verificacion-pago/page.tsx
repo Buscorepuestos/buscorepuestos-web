84fc6566753bd3fe9746c465c6113f6b26803d65
Yuniel Lavin <yuniel.lavin@buscorepuestos.com>
2024-07-15 13:12:18 -0400

    feat: [BR-479] add section Personal Info to payment verification form

--- app/verificacion-pago/page.tsx
+++ app/verificacion-pago/page.tsx
@@ -2,6 +2,7 @@
 import ShoppingBasket from '../core/components/shopping-cart/ShoppingBasket'
 import React from 'react'
 import { p } from 'vitest/dist/reporters-yx5ZTtEV'
+import Input from '../core/components/input/input'
 
 export default function Payment() {
 
@@ -58,7 +59,7 @@ export default function Payment() {
 		}
 	]
 	return (
-		<section className={'w-full bg-green-300 mt-[12vw] mb-[8vw] '}>
+		<section className={'w-full bg-green-300 mt-[12vw] mb-[8vw] p-6'}>
 			<article>
 				<ShoppingBasket products={products} />
 				<div className="w-full h-[2px] bg-secondary-blue mt-6" />
@@ -66,8 +67,24 @@ export default function Payment() {
 					<p className={'mr-6 font-medium'}>Total:</p>
 					<p className={'text-title-3 text-primary-blue-2 font-semibold'}>{products.map(item => item.price).reduce((acc, curr) => acc + curr, 0)}€</p>
 				</div>
-				<div className="w-full h-[2px] bg-secondary-blue mb-6" />
+				<div className="w-full h-[2px] bg-secondary-blue mb-8" />
 			</article>
+			<article>
+				<form className={'flex flex-col justify-center items-center'}>
+					<h1 className={'text-title-3 mb-6 self-start'}>Información personal</h1>
+					<div className={'grid grid-cols-1 w-[60%] gap-4'}>
+						<Input placeholder={'Nombre y apellidos'} name={'name'} value={''}/>
+					</div>
+					<div className={'grid grid-cols-3 w-[60%] mt-4 gap-4'}>
+						<Input placeholder={'Email'} name={'email'} value={''} cssClass={'col-span-2'} />
+						<Input placeholder={'NIF / CIF'} name={'company_id'} value={''}/>
+					</div>
+					<div className={'grid grid-cols-2 w-[60%] mt-4 gap-4'}>
+						<Input placeholder={'Número de teléfono'} name={'phone'} value={''}/>
+					</div>
+				</form>
+			</article>
+
 		</section>
 	)
 }
