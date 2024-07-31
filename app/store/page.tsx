54ea6478fdbcf1c02466c7821ec9abbcaac337ad
Jhoskar Toro <jhoskartoro@gmail.com>
2024-07-31 17:06:49 -0400

    refactor: :zap: [BR-479] refactor store

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,6 +1,7 @@
 'use client'
-import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import CardPrice from '../core/components/cards/CardPrice'
+import { getProducts } from '../services/products/products.service'
 import { IProductMongoose } from '../types/product'
 import algoliasearch from 'algoliasearch'
 import SearchBar from '../core/components/SearchBar'
@@ -9,49 +10,33 @@ const appID = 'DSKGGHHS58'
 const apiKey = '6f49eeb288faef802bf5236c9fa6720d'
 
 export default function Store() {
+
 	const client = algoliasearch(appID, apiKey)
+	// Create a new index and add a record
 	const index = client.initIndex('dev_PRODUCTS')
-	const hitsRef = useRef<IProductMongoose[]>([])
+	let hits: any = [];
 
 	const search = async (query: string) => {
-		const result = await index.search(query)
-		setProducts(result.hits as unknown as IProductMongoose[])
+		// Search for query in the index "products"
+		const result = await index.search(query);
+		setProducts(result.hits as unknown as IProductMongoose[]);
 	}
 
 	const [products, setProducts] = useState<IProductMongoose[]>([])
 	const [loading, setLoading] = useState(true)
 	const [error, setError] = useState<string | null>(null)
-	const [inputValue, setInputValue] = useState<string>('')
+	const [inputValue, setInputValue] = useState<string>('');
 
 	useEffect(() => {
 		const fetchProducts = async () => {
 			try {
-				await index.browseObjects({
-					batch: (batch) => {
-						const formattedBatch = batch.map((item: any) => ({
-							...item,
-							// Asegúrate de que los campos necesarios se ajusten al tipo `IProductMongoose`
-							title: item.title,
-							mainReference: item.mainReference,
-							brand: item.brand,
-							articleModel: item.articleModel,
-							year: item.year,
-							buscorepuestosPrice: item.buscorepuestosPrice,
-							images: item.images,
-						}))
-						hitsRef.current = hitsRef.current.concat(formattedBatch)
+				index.browseObjects({
+					batch: batch => {
+						// eslint-disable-next-line react-hooks/exhaustive-deps
+						hits = hits.concat(batch)
 					},
-					attributesToRetrieve: [
-						'title',
-						'mainReference',
-						'brand',
-						'articleModel',
-						'year',
-						'buscorepuestosPrice',
-						'images',
-					],
-				})
-				setProducts(hitsRef.current)
+					attributesToRetrieve: ['title', 'mainReference', 'brand', 'articleModel', 'year', 'buscorepuestosPrice', 'images'],
+				}).then(() => setProducts(hits))
 			} catch (error) {
 				setError((error as Error).message)
 			} finally {
@@ -60,51 +45,33 @@ export default function Store() {
 		}
 
 		fetchProducts()
-	}, [index])
+	}, [])
 
 	const cleanValue = (text: string) => {
 		return `${' ' + text.replace('-', '')}`
 	}
 	const parsePrice = (price: number) => {
-		return price.toFixed(2).replace('.', ',')
+		return price.toFixed(2).replace('.', ',');
 	}
 
 	const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-		setInputValue(event.target.value)
-		search(event.target.value)
-	}
+		setInputValue(event.target.value);
+		search(event.target.value);
+	};
 
 	return (
 		<main>
 			<div className={'flex justify-end mr-6 mt-80'}>
-				<SearchBar
-					onChange={handleInputChange}
-					height={'52px'}
-					width={'w-[480px] mobile:w-[80vw]'}
-					borderColor={'#12B1BB'}
-					borderWidth={'2px'}
-				/>
+				<SearchBar onChange={handleInputChange} height={'52px'} width={'w-[480px] mobile:w-[80vw]'} borderColor={'#12B1BB'} borderWidth={'2px'}/>
 			</div>
 			<section
-				className={
-					'grid grid-cols-4 grid-rows-4 tablet:grid-cols-3 tablet:grid-rows-3 mobile:grid-cols-2 mobile:grid-rows-2'
-				}
-			>
-				{products.map((product, index) => (
-					<CardPrice
-						key={index}
-						title={product.title}
-						reference={product.mainReference ?? ''}
-						description={`${cleanValue(product.brand)}${cleanValue(product.articleModel)}${cleanValue(product.year.toString())}`}
-						price={Number(
-							parsePrice(product.buscorepuestosPrice ?? 0)
-						)}
-						image={
-							product.images[0]
-								? product.images[0]
-								: '/nodisponible.png'
-						}
-					/>
+				className={'grid grid-cols-4 grid-rows-4 tablet:grid-cols-3 tablet:grid-rows-3 mobile:grid-cols-2 mobile:grid-rows-2'}>
+				{products.map((product: any, index) => (
+					<CardPrice key={index} title={product.title}
+							   reference={product.mainReference}
+							   description={`${cleanValue(product.brand)}${cleanValue(product.articleModel)}${cleanValue(product.year.toString())}`}
+							   price={(product?.buscorepuestosPrice || 0).toFixed(2)}
+							   image={product.images[0] ? product.images[0] : '/nodisponible.png'} />
 				))}
 				{loading && <p>Loading...</p>}
 				{error && <p>Error</p>}
