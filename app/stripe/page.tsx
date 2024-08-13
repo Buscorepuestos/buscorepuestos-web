c5b5b055a0163860143e21ce53e2c5d06346f0c4
Jhoskar Toro <jhoskartoro@gmail.com>
2024-08-13 10:29:00 -0400

    fix: :zap: [BR-558] componente /stripe/page no se utiliza

--- app/stripe/page.tsx
+++ app/stripe/page.tsx
@@ -1,48 +1,43 @@
-'use client'
-import { useEffect, useState } from 'react'
-import { createPaymentIntent } from '../services/checkout/stripe.service'
-import PaymentForm from '../core/components/checkout/PaymentForm'
-import { FormsFields } from '../verificacion-pago/page'
+// 'use client'
+// import { useEffect, useState } from 'react'
+// import { createPaymentIntent } from '../services/checkout/stripe.service'
+// import PaymentForm from '../core/components/checkout/PaymentForm'
+// import { FormsFields } from '../verificacion-pago/page'
 
-interface AppProps {
-	fieldsValues: FormsFields
-	purchaseIds: string[]
-}
+// export default function App(fieldsValues:FormsFields, purchaseIds:string[]) {
 
-export default function App({ fieldsValues, purchaseIds }: AppProps) {
+// 	const [clientSecret, setClientSecret] = useState<string>('')
+// 	const [error, setError] = useState<string>('')
 
-	const [clientSecret, setClientSecret] = useState<string>('')
-	const [error, setError] = useState<string>('')
+// 	useEffect(() => {
+// 		const createIntent = async () => {
+// 			try {
+// 				// Mock data
+// 				const res = await createPaymentIntent({
+// 					amount: 200,
+// 					currency: 'eur',
+// 					cartIDs: ['recV9AQVCb64NveMF'],
+// 					automatic_payment_methods: { enabled: true },
+// 				});
+// 				setClientSecret(res.data.client_secret);
+// 			} catch (error) {
+// 				if (error instanceof Error) {
+// 					setError(error.message);
+// 				} else {
+// 					setError('An unexpected error occurred');
+// 				}
+// 			}
+// 		}
 
-	useEffect(() => {
-		const createIntent = async () => {
-			try {
-				// Mock data
-				const res = await createPaymentIntent({
-					amount: 200,
-					currency: 'eur',
-					cartIDs: ['recV9AQVCb64NveMF'],
-					automatic_payment_methods: { enabled: true },
-				});
-				setClientSecret(res.data.client_secret);
-			} catch (error) {
-				if (error instanceof Error) {
-					setError(error.message);
-				} else {
-					setError('An unexpected error occurred');
-				}
-			}
-		}
-
-		createIntent();
-	}, [])
+// 		createIntent();
+// 	}, [])
 
 
-	return (
-		<div>
-			{clientSecret && <PaymentForm fieldsValues={fieldsValues} purchaseIds={purchaseIds} clientSecret={clientSecret} />}
-			{error && <p>Error: {error}</p>}
-		</div>
-	)
-};
+// 	return (
+// 		<div>
+// 			{clientSecret && <PaymentForm fieldsValues={fieldsValues} purchaseIds={purchaseIds} clientSecret={clientSecret} />}
+// 			{error && <p>Error: {error}</p>}
+// 		</div>
+// 	)
+// };
 
