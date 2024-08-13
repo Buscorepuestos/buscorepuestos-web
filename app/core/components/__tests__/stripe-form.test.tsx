40aa1f67c47f83153f4cff94cbec758ab066d1c8
Jhoskar Toro <jhoskartoro@gmail.com>
2024-08-12 20:54:17 -0400

    fix: :zap: [BR-558] omision de test de stripe-forms

--- app/core/components/__tests__/stripe-form.test.tsx
+++ app/core/components/__tests__/stripe-form.test.tsx
@@ -1,14 +1,20 @@
-import { render, screen, fireEvent, waitFor, cleanup } from '@testing-library/react';
-import { describe, test, expect, vi, beforeEach, afterEach } from 'vitest';
-import { useElements, useStripe } from '@stripe/react-stripe-js';
-import { createBill } from '../../../services/billing/billing.service';
-import StripeForm from '../checkout/StripeForm';
-import { configureStore } from '@reduxjs/toolkit';
-import shoppingCartReducer from '../../../redux/features/shoppingCartSlice';
-import { Provider } from 'react-redux';
+import {
+	render,
+	screen,
+	fireEvent,
+	waitFor,
+	cleanup,
+} from '@testing-library/react'
+import { describe, test, expect, vi, beforeEach, afterEach } from 'vitest'
+import { useElements, useStripe } from '@stripe/react-stripe-js'
+import { createBill } from '../../../services/billing/billing.service'
+import StripeForm from '../checkout/StripeForm'
+import { configureStore } from '@reduxjs/toolkit'
+import shoppingCartReducer from '../../../redux/features/shoppingCartSlice'
+import { Provider } from 'react-redux'
 
 const fieldValues = {
-  name: 'Carlos',
+	name: 'Carlos',
 	email: 'carlos@gmail.com',
 	nif: '3456fg',
 	phoneNumber: '66666666',
@@ -27,74 +33,88 @@ const fieldValues = {
 const purchaseId = 'recV9AQVCb64NveMF'
 
 vi.mock('@stripe/react-stripe-js', () => ({
-  PaymentElement: vi.fn(() => <div data-testid="payment-element" />),
-  useElements: vi.fn(),
-  useStripe: vi.fn(),
-}));
+	PaymentElement: vi.fn(() => <div data-testid="payment-element" />),
+	useElements: vi.fn(),
+	useStripe: vi.fn(),
+}))
 
 vi.mock('../../../services/billing/billing.service', () => ({
-  createBill: vi.fn(),
-}));
+	createBill: vi.fn(),
+}))
 
-const mockUseElements = useElements as vi.Mock;
-const mockUseStripe = useStripe as vi.Mock;
-const mockCreateBill = createBill as vi.Mock;
+const mockUseElements = useElements as vi.Mock
+const mockUseStripe = useStripe as vi.Mock
+const mockCreateBill = createBill as vi.Mock
 
 // Crear un store mock
 const createMockStore = (initialState = {}) => {
-  return configureStore({
-    reducer: {
-      shoppingCart: shoppingCartReducer,
-    },
-    preloadedState: initialState,
-  });
-};
+	return configureStore({
+		reducer: {
+			shoppingCart: shoppingCartReducer,
+		},
+		preloadedState: initialState,
+	})
+}
 
 // Modificar la función render para envolver en Provider
 const renderWithProvider = (ui: React.ReactElement, initialState = {}) => {
-  const store = createMockStore(initialState);
-  return render(<Provider store={store}>{ui}</Provider>);
-};
+	const store = createMockStore(initialState)
+	return render(<Provider store={store}>{ui}</Provider>)
+}
 
 describe('StripeForm', () => {
-  beforeEach(() => {
-    mockUseElements.mockReturnValue({});
-    mockUseStripe.mockReturnValue({
-      retrievePaymentIntent: vi.fn().mockResolvedValue({
-        paymentIntent: { id: 'pi_123', status: 'succeeded' },
-      }),
-      confirmPayment: vi.fn().mockResolvedValue({}),
-    });
-    mockCreateBill.mockResolvedValue({});
-  });
-  
-  afterEach(() => {
-    vi.clearAllMocks();
-    cleanup();
-  });
+	beforeEach(() => {
+		mockUseElements.mockReturnValue({})
+		mockUseStripe.mockReturnValue({
+			retrievePaymentIntent: vi.fn().mockResolvedValue({
+				paymentIntent: { id: 'pi_123', status: 'succeeded' },
+			}),
+			confirmPayment: vi.fn().mockResolvedValue({}),
+		})
+		mockCreateBill.mockResolvedValue({})
+	})
 
-  test('should handle form submission when stripe is undefined', async () => {
-    // Mock useStripe to return null
-    mockUseStripe.mockReturnValueOnce(null);
+	afterEach(() => {
+		vi.clearAllMocks()
+		cleanup()
+	})
 
-    renderWithProvider(<StripeForm purchaseIds={[purchaseId]} fieldsValues={fieldValues} clientSecret="test_secret" label={'Pagar ahora'}/>);
+	test('should handle form submission when stripe is undefined', async () => {
+		// Mock useStripe to return null
+		mockUseStripe.mockReturnValueOnce(null)
 
-    const submitButton = screen.getByText('Pagar ahora');
-    fireEvent.click(submitButton);
+		renderWithProvider(
+			<StripeForm
+				purchaseIds={[purchaseId]}
+				fieldsValues={fieldValues}
+				clientSecret="test_secret"
+				label={'Pagar ahora'}
+			/>
+		)
 
-    // Ensure createBill is not called since stripe is null
-    expect(mockCreateBill).not.toHaveBeenCalled();
+		const submitButton = screen.getByText('Pagar ahora')
+		fireEvent.click(submitButton)
 
-    // Ensure no error messages or other side effects occur
-    expect(screen.queryByText('Payment succeeded!')).toBeNull();
-    expect(screen.queryByText('An unexpected error occurred.')).toBeNull();
-  });
+		// Ensure createBill is not called since stripe is null
+		expect(mockCreateBill).not.toHaveBeenCalled()
 
-  test('should render payment form', () => {
-    const { getByText, getByTestId } = renderWithProvider(<StripeForm purchaseIds={[purchaseId]} fieldsValues={fieldValues} clientSecret="test_secret" label={'Pagar ahora'} />);
-    expect(getByTestId('payment-element')).not.toBeNull();
-    expect(getByText('Pagar ahora')).not.toBeNull();
-  });
+		// Ensure no error messages or other side effects occur
+		expect(screen.queryByText('Payment succeeded!')).toBeNull()
+		expect(screen.queryByText('An unexpected error occurred.')).toBeNull()
+	})
+
+	test('should render payment form', () => {
+		const { getByText, getByTestId } = renderWithProvider(
+			<StripeForm
+				purchaseIds={[purchaseId]}
+				fieldsValues={fieldValues}
+				clientSecret="test_secret"
+				label={'Pagar ahora'}
+			/>
+		)
+		expect(getByTestId('payment-element')).not.toBeNull()
+		expect(getByText('Pagar ahora')).not.toBeNull()
+	})
 
 	test('should handle form submission', async () => {
 		mockUseStripe.mockReturnValue({
@@ -104,14 +124,20 @@ describe('StripeForm', () => {
 			confirmPayment: vi.fn().mockResolvedValue({}),
 		})
 
-		const { getByText } = renderWithProvider(<StripeForm purchaseIds={[purchaseId]} fieldsValues={fieldValues} clientSecret="test_secret" label={'Pagar ahora'}/>)
+		const { getByText } = renderWithProvider(
+			<StripeForm
+				purchaseIds={[purchaseId]}
+				fieldsValues={fieldValues}
+				clientSecret="test_secret"
+				label={'Pagar ahora'}
+			/>
+		)
 
 		const submitButton = screen.getByText('Pagar ahora')
 		fireEvent.click(submitButton)
 
 		setTimeout(() => {
-			expect(mockCreateBill).toHaveBeenCalledWith(
-				{
+			expect(mockCreateBill).toHaveBeenCalledWith({
 				'Id Pago Stripe': undefined,
 				Compras: ['recV9AQVCb64NveMF'],
 				Usuarios: ['recxNb1bKbkcrueq1'],
@@ -125,136 +151,180 @@ describe('StripeForm', () => {
 				nif: '3456fg',
 				phone: 66666666,
 				province: 'Las Palmas',
-
-				})
-        expect(getByText('Payment succeeded!')).toBeDefined()
-		} ,500)
+			})
+			expect(getByText('Payment succeeded!')).toBeDefined()
+		}, 500)
 	})
 
-  test('should handle errors in form submission', async () => {
-    mockUseStripe.mockReturnValueOnce({
-      retrievePaymentIntent: vi.fn().mockResolvedValue({
-        paymentIntent: { id: 'pi_123', status: 'requires_payment_method' },
-      }),
-      confirmPayment: vi.fn().mockResolvedValue({
-        error: { type: 'card_error', message: 'Card error' },
-      }),
-    });
+	test('should handle errors in form submission', async () => {
+		mockUseStripe.mockReturnValueOnce({
+			retrievePaymentIntent: vi.fn().mockResolvedValue({
+				paymentIntent: {
+					id: 'pi_123',
+					status: 'requires_payment_method',
+				},
+			}),
+			confirmPayment: vi.fn().mockResolvedValue({
+				error: { type: 'card_error', message: 'Card error' },
+			}),
+		})
 
-    renderWithProvider(<StripeForm purchaseIds={[purchaseId]} fieldsValues={fieldValues} clientSecret="test_secret" label={'Pagar ahora'} />);
+		renderWithProvider(
+			<StripeForm
+				purchaseIds={[purchaseId]}
+				fieldsValues={fieldValues}
+				clientSecret="test_secret"
+				label={'Pagar ahora'}
+			/>
+		)
 
-    const submitButton = screen.getByText('Pagar ahora');
-    fireEvent.click(submitButton);
+		const submitButton = screen.getByText('Pagar ahora')
+		fireEvent.click(submitButton)
 
-    setTimeout(() => {
-      expect(screen.queryByText('Card error')).not.toBeNull();
-    }, 500);
-  });
+		setTimeout(() => {
+			expect(screen.queryByText('Card error')).not.toBeNull()
+		}, 500)
+	})
 
-  test('should handle processing payment status', async () => {
-    mockUseStripe.mockReturnValueOnce({
-      retrievePaymentIntent: vi.fn().mockResolvedValue({
-        paymentIntent: { id: 'pi_123', status: 'processing' },
-      }),
-      confirmPayment: vi.fn().mockResolvedValue({}),
-    });
+	test('should handle processing payment status', async () => {
+		mockUseStripe.mockReturnValueOnce({
+			retrievePaymentIntent: vi.fn().mockResolvedValue({
+				paymentIntent: { id: 'pi_123', status: 'processing' },
+			}),
+			confirmPayment: vi.fn().mockResolvedValue({}),
+		})
 
-    renderWithProvider(<StripeForm purchaseIds={[purchaseId]} fieldsValues={fieldValues} clientSecret="test_secret" label={'Pagar ahora'} />);
+		renderWithProvider(
+			<StripeForm
+				purchaseIds={[purchaseId]}
+				fieldsValues={fieldValues}
+				clientSecret="test_secret"
+				label={'Pagar ahora'}
+			/>
+		)
 
-    const submitButton = screen.getByText('Pagar ahora');
-    fireEvent.click(submitButton);
+		const submitButton = screen.getByText('Pagar ahora')
+		fireEvent.click(submitButton)
 
-    setTimeout(() => {
-      expect(screen.queryByText('Your payment is processing.')).not.toBeNull();
-    },500);
-  });
+		setTimeout(() => {
+			expect(
+				screen.queryByText('Your payment is processing.')
+			).not.toBeNull()
+		}, 500)
+	})
 
-  test('should handle another payment status', async () => {
-    mockUseStripe.mockReturnValueOnce({
-      retrievePaymentIntent: vi.fn().mockResolvedValue({
-        paymentIntent: { id: 'pi_123', status: 'another status' },
-      }),
-      confirmPayment: vi.fn().mockResolvedValue({}),
-    });
+	test('should handle another payment status', async () => {
+		mockUseStripe.mockReturnValueOnce({
+			retrievePaymentIntent: vi.fn().mockResolvedValue({
+				paymentIntent: { id: 'pi_123', status: 'another status' },
+			}),
+			confirmPayment: vi.fn().mockResolvedValue({}),
+		})
 
-    renderWithProvider(<StripeForm purchaseIds={[purchaseId]} fieldsValues={fieldValues} clientSecret="test_secret" label={'Pagar ahora'} />);
+		renderWithProvider(
+			<StripeForm
+				purchaseIds={[purchaseId]}
+				fieldsValues={fieldValues}
+				clientSecret="test_secret"
+				label={'Pagar ahora'}
+			/>
+		)
 
-    const submitButton = screen.getByText('Pagar ahora');
-    fireEvent.click(submitButton);
+		const submitButton = screen.getByText('Pagar ahora')
+		fireEvent.click(submitButton)
 
-    setTimeout(() => {
-      expect(screen.queryByText('Something went wrong.')).not.toBeNull();
-    },500);
-  });
+		setTimeout(() => {
+			expect(screen.queryByText('Something went wrong.')).not.toBeNull()
+		}, 500)
+	})
 
-  test('should handle unexpected error', async () => {
-    mockUseStripe.mockReturnValueOnce({
-      retrievePaymentIntent: vi.fn().mockResolvedValue({
-        paymentIntent: { id: 'pi_123', status: 'another status' },
-      }),
-      confirmPayment: vi.fn().mockResolvedValue({
-        error: { type: 'error-x', message: 'Unexpected Error' },
-      }),
-    });
+	test('should handle unexpected error', async () => {
+		mockUseStripe.mockReturnValueOnce({
+			retrievePaymentIntent: vi.fn().mockResolvedValue({
+				paymentIntent: { id: 'pi_123', status: 'another status' },
+			}),
+			confirmPayment: vi.fn().mockResolvedValue({
+				error: { type: 'error-x', message: 'Unexpected Error' },
+			}),
+		})
 
-    renderWithProvider(<StripeForm purchaseIds={[purchaseId]} fieldsValues={fieldValues} clientSecret="test_secret" label={'Pagar ahora'} />);
+		renderWithProvider(
+			<StripeForm
+				purchaseIds={[purchaseId]}
+				fieldsValues={fieldValues}
+				clientSecret="test_secret"
+				label={'Pagar ahora'}
+			/>
+		)
 
-    const submitButton = screen.getByText('Pagar ahora');
-    fireEvent.click(submitButton);
+		const submitButton = screen.getByText('Pagar ahora')
+		fireEvent.click(submitButton)
 
-    setTimeout(() => {
-      expect(screen.queryByText('An unexpected error occurred.')).not.toBeNull();
-    },500);
-  });
+		setTimeout(() => {
+			expect(
+				screen.queryByText('An unexpected error occurred.')
+			).not.toBeNull()
+		}, 500)
+	})
 
-  test('should not submit the form if elements is null', async () => {
-    mockUseElements.mockReturnValueOnce(null);
+	test('should not submit the form if elements is null', async () => {
+		mockUseElements.mockReturnValueOnce(null)
 
-    renderWithProvider(<StripeForm purchaseIds={[purchaseId]} fieldsValues={fieldValues} clientSecret="test_secret" label={'Pagar ahora'}/>);
+		renderWithProvider(
+			<StripeForm
+				purchaseIds={[purchaseId]}
+				fieldsValues={fieldValues}
+				clientSecret="test_secret"
+				label={'Pagar ahora'}
+			/>
+		)
 
-    const submitButton = screen.getByText('Pagar ahora');
-    fireEvent.click(submitButton);
+		const submitButton = screen.getByText('Pagar ahora')
+		fireEvent.click(submitButton)
 
-    // Ensure createBill is not called since elements is null
-    expect(mockCreateBill).not.toHaveBeenCalled();
+		// Ensure createBill is not called since elements is null
+		expect(mockCreateBill).not.toHaveBeenCalled()
 
-    // Ensure no error messages or other side effects occur
-    expect(screen.queryByText('Payment succeeded!')).toBeNull();
-    expect(screen.queryByText('An unexpected error occurred.')).toBeNull();
-  });
+		// Ensure no error messages or other side effects occur
+		expect(screen.queryByText('Payment succeeded!')).toBeNull()
+		expect(screen.queryByText('An unexpected error occurred.')).toBeNull()
+	})
 
-  test('should show spinner when loading', async () => {
-	mockUseStripe.mockReturnValue({
-	  retrievePaymentIntent: vi.fn().mockResolvedValue({
-		paymentIntent: { id: 'pi_123', status: 'succeeded' },
-	  }),
-	  confirmPayment: vi.fn().mockImplementation(() => {
-		return new Promise((resolve) => {
-		  setTimeout(() => {
-			resolve({});
-		  }, 100); // Simulate a delay
-		});
-	  }),
-	});
-  
-	const { getByText, queryByTestId } = renderWithProvider(
-	  <StripeForm purchaseIds={[purchaseId]} fieldsValues={fieldValues} clientSecret="test_secret" label={'Pagar ahora'} />
-	);
-  
-	const submitButton = getByText('Pagar ahora');
-	fireEvent.click(submitButton);
-  
-	// Verify the spinner is displayed while loading
-	setTimeout(() => {
-	  const spinner = queryByTestId('spinner');
-	  expect(spinner).not.toBeNull();
-	},500);
-  
-	// Optionally, you can also test that the spinner disappears after loading
-	setTimeout(() => {
-	  expect(queryByTestId('spinner')).toBeNull();
-	},500);
-  });
-  
+	test('should show spinner when loading', async () => {
+		mockUseStripe.mockReturnValue({
+			retrievePaymentIntent: vi.fn().mockResolvedValue({
+				paymentIntent: { id: 'pi_123', status: 'succeeded' },
+			}),
+			confirmPayment: vi.fn().mockImplementation(() => {
+				return new Promise((resolve) => {
+					setTimeout(() => {
+						resolve({})
+					}, 100) // Simulate a delay
+				})
+			}),
+		})
 
-});
+		const { getByText, queryByTestId } = renderWithProvider(
+			<StripeForm
+				purchaseIds={[purchaseId]}
+				fieldsValues={fieldValues}
+				clientSecret="test_secret"
+				label={'Pagar ahora'}
+			/>
+		)
+
+		const submitButton = getByText('Pagar ahora')
+		fireEvent.click(submitButton)
+
+		// Verify the spinner is displayed while loading
+		setTimeout(() => {
+			const spinner = queryByTestId('spinner')
+			expect(spinner).not.toBeNull()
+		}, 500)
+
+		// Optionally, you can also test that the spinner disappears after loading
+		setTimeout(() => {
+			expect(queryByTestId('spinner')).toBeNull()
+		}, 500)
+	})
+})
