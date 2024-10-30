aae4b34543c22a808fd0318e6cf8ea66ee2fa8b8
Jhoskar Toro <jhoskartoro@gmail.com>
2024-10-30 14:05:53 -0300

    feat: :zap: [BR-598] arreglos de diseño y testing

--- app/core/components/transferPayment/transferPayment.tsx
+++ app/core/components/transferPayment/transferPayment.tsx
@@ -279,12 +279,14 @@ const TransferPayment: React.FC<TransferPaymentProps> = ({
 					<PDFViewer pdfUrl={previewUrl} />
 				)}
 			</div>
-			<button
-				onClick={handlePayment}
-				className="mt-4 inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-			>
-				Finalizar compra
-			</button>
+			<div className='flex justify-end'>
+				<button
+					onClick={handlePayment}
+					className="mt-4 inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+				>
+					Finalizar compra
+				</button>
+			</div>
 		</div>
 	)
 }
