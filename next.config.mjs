99e11607b9865f9dea15cc59d40a04868c50936d
Jhoskar Toro <jhoskartoro@gmail.com>
2024-12-03 15:27:28 -0300

    fix: :zap: cdn16 imagenes de metasync

--- next.config.mjs
+++ next.config.mjs
@@ -28,6 +28,12 @@ const nextConfig = {
 				port: '',
 				pathname: '**',	
 			},
+			{
+				protocol: 'https',
+				hostname: 'cdn16.metasync.com',
+				port: '',
+				pathname: '**',	
+			},
 		],
 	},
 }
