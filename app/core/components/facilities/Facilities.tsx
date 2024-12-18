8f708d36a1b4de7cd40ae6c21f4d1397191d444f
Jhoskar Toro <jhoskartoro@gmail.com>
2024-12-18 16:17:40 -0300

    style: :art: [BR-59] fix de estilos de tienda

--- app/core/components/facilities/Facilities.tsx
+++ app/core/components/facilities/Facilities.tsx
@@ -20,7 +20,7 @@ const Facilities: React.FC<FacilitiesProps> = ({ classNamePrincipal, classNameIm
 					height={34}
 					className={classNameImg}
 				/>
-				<p className={` ${isProductPage && 'mobile:text-[3vw]' } lg:text-[1vw] md:text-[1.5vw] font-normal mobile:font-bold`}>2 años de garantía</p>
+				<p className={` ${isProductPage && 'mobile:text-[3vw] lg:text-[1vw] md:text-[1.5vw]' } font-normal mobile:font-bold`}>2 años de garantía</p>
 			</div>
 			<div className={` ${isProductPage ? 'mobile:gap-2' : 'mobile:gap-5'} flex items-center md:gap-4 sm:gap-1 `}>
 				<Image
@@ -30,7 +30,7 @@ const Facilities: React.FC<FacilitiesProps> = ({ classNamePrincipal, classNameIm
 					height={34}
 					className={classNameImg}
 				/>
-				<p className={` ${isProductPage && 'mobile:text-[3vw]' } lg:text-[1vw] md:text-[1.5vw] font-normal mobile:font-bold`}>devolución gratuita</p>
+				<p className={` ${isProductPage && 'mobile:text-[3vw] lg:text-[1vw] md:text-[1.5vw]' } font-normal mobile:font-bold`}>devolución gratuita</p>
 			</div>
 			<div className={` ${isProductPage && 'mobile:hidden'} flex items-center md:gap-4 sm:gap-1 mobile:gap-5`}>
 				<Image
@@ -40,7 +40,7 @@ const Facilities: React.FC<FacilitiesProps> = ({ classNamePrincipal, classNameIm
 					height={34}
 					className={classNameImg}
 				/>
-				<p className="lg:text-[1vw] md:text-[1.5vw] font-normal mobile:font-bold">Atencíon al cliente 24h</p>
+				<p className={` ${isProductPage && 'lg:text-[1vw] md:text-[1.5vw]'} font-normal mobile:font-bold `}>Atencíon al cliente 24h</p>
 			</div>
 			<div className={` ${isProductPage && 'mobile:hidden'} flex items-center md:gap-4 sm:gap-1 mobile:gap-5`}>
 				<Image
@@ -50,7 +50,7 @@ const Facilities: React.FC<FacilitiesProps> = ({ classNamePrincipal, classNameIm
 					height={34}
 					className={classNameImg}
 				/>
-				<p className="lg:text-[1vw] md:text-[1.5vw] font-normal mobile:font-bold">Pago a plazos</p>
+				<p className={` ${isProductPage && 'lg:text-[1vw] md:text-[1.5vw]'} font-normal mobile:font-bold `}>Pago a plazos</p>
 			</div>
 		</div>
 	)
