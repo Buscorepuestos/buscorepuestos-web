87a19d2284f4880d2bfb4171263d842edf03290f
Jhoskar Toro <jhoskartoro@gmail.com>
2025-03-27 11:27:34 -0300

    fix: :bug: update product ID handling to remove unnecessary $oid references

--- app/page.tsx
+++ app/page.tsx
@@ -652,9 +652,9 @@ export default function Home() {
 										? product.images[0]
 										: '/nodisponible.png'
 								}
-								handle={() => handle(product._id.$oid)}
-								id={product._id.$oid}
-								loading={loadingPurchase === product._id.$oid}
+								handle={() => handle(product._id)}
+								id={product._id}
+								loading={loadingPurchase === product._id}
 							/>
 						</SwiperSlide>
 					))}
@@ -723,9 +723,9 @@ export default function Home() {
 										? product.images[0]
 										: '/nodisponible.png'
 								}
-								handle={() => handle(product._id.$oid)}
-								id={product._id.$oid}
-								loading={loadingPurchase === product._id.$oid}
+								handle={() => handle(product._id)}
+								id={product._id}
+								loading={loadingPurchase === product._id}
 							/>
 						</SwiperSlide>
 					))}
